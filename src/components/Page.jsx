ce07676ae7aeb1c4f86cd9c2492e54e12ab9277d
Benjamin Wasilewski <ben.wasilewski@apervita.com>
2020-02-22 21:59:12 -0600

    create a base page component

---
+++ src/components/Page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from 'bloomer'
+
+const Page = props => {
+  return (
+    <Container>
+      { props.children }
+    </Container>
+  )
+}
+
+export default Page;
